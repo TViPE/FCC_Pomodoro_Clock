61561adf90e901f3de457aa6bb3a454716919a87
TRI VI <trivi2710@gmail.com>
2017-05-26 11:46:57 -0400

    Create function for button to increase/decrease break length and session length
    Feat:
    
    Create function for button to increase/decrease break length and
    session length

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 var app = angular.module("pomodoroApp", []);
-app.controller("pomodoroCtrl", function ($scope) {
-
+app.controller("pomodoroCtrl", function ($scope, $interval) {
+	$scope.breaktime = 5;
+	$scope.sessiontime = 25;
+	$scope.minusBreakTime = function(){
+		if($scope.breaktime < 1) {
+			$scope.breaktime = 1;
+		}
+		$scope.breaktime--;
+	}
+	$scope.plusBreakTime = function(){
+		$scope.breaktime++;
+	}
+	$scope.minusSessionTime = function(){
+		if($scope.sessiontime < 1) {
+			$scope.sessiontime = 1;
+		}
+		$scope.sessiontime--;
+	}
+	$scope.plusSessionTime = function(){
+		$scope.sessiontime++;
+	}
 })

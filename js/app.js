16bd917a20f9cef41d2ba1dbde51607b11dc6482
TRI VI <trivi2710@gmail.com>
2017-05-26 17:41:38 -0400

    Feat: writing function to count down session length
    Feat: session length counting down function
    
    1. Clicking inside the border section will toggle the function counting
    down session length

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ var app = angular.module("pomodoroApp", []);
 app.controller("pomodoroCtrl", function ($scope, $interval) {
 	$scope.breaktime = 5;
 	$scope.sessiontime = 25;
+	$scope.sessionName = "Session";
+	$scope.isRunning = false;
+	$scope.timeLeft = $scope.sessiontime; 
+	var promise;
 	$scope.minusBreakTime = function(){
 		if($scope.breaktime < 1) {
 			$scope.breaktime = 1;
@@ -20,4 +24,26 @@ app.controller("pomodoroCtrl", function ($scope, $interval) {
 	$scope.plusSessionTime = function(){
 		$scope.sessiontime++;
 	}
+
+	$scope.minusTimeLeft = function(){
+		$scope.timeLeft--;
+	}
+
+	//promise =$interval($scope.minusTimeLeft(), 1000);
+	$scope.toggleTimer = function() {
+		
+		$scope.isRunning = !($scope.isRunning);
+		console.log("A: " + $scope.isRunning);
+		if($scope.isRunning == true){
+			$scope.minusTimeLeft();
+			promise = $interval($scope.minusTimeLeft, 1000);
+		} else if($scope.isRunning === false) {
+			$interval.cancel(promise);
+		}
+		
+		
+	}
+
+
+
 })

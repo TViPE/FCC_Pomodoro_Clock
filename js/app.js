3624fa1351c5809127fcfc1ee7ab82f000a4571b
TRI VI <trivi2710@gmail.com>
2017-05-27 17:25:54 -0400

    Write a function to update timer when each session ends
    Feat: Write a function to update timer when each session ends

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 var app = angular.module("pomodoroApp", []);
 app.controller("pomodoroCtrl", function ($scope, $interval) {
-	$scope.breaktime = 5;
-	$scope.sessiontime = 25;
+	$scope.breakTime = 5;
+	$scope.sessionTime = 25;
 	$scope.sessionName = "Session";
+	$scope.breakName = "Break";
 	$scope.isRunning = false;
-	$scope.timeLeft = $scope.sessiontime; 
+	$scope.timeLeft = $scope.sessionTime; 
+	$scope.currentName = $scope.sessionName;
 	var promise;
+
+	// Button
 	$scope.minusBreakTime = function(){
 		if($scope.breaktime < 1) {
 			$scope.breaktime = 1;
@@ -24,6 +28,7 @@ app.controller("pomodoroCtrl", function ($scope, $interval) {
 	$scope.plusSessionTime = function(){
 		$scope.sessiontime++;
 	}
+	//-----------------------------//
 
 	$scope.minusTimeLeft = function(){
 		$scope.timeLeft--;
@@ -31,18 +36,64 @@ app.controller("pomodoroCtrl", function ($scope, $interval) {
 
 	//promise =$interval($scope.minusTimeLeft(), 1000);
 	$scope.toggleTimer = function() {
-		
 		$scope.isRunning = !($scope.isRunning);
-		console.log("A: " + $scope.isRunning);
 		if($scope.isRunning == true){
-			$scope.minusTimeLeft();
-			promise = $interval($scope.minusTimeLeft, 1000);
+			//promise = $interval($scope.minusTimeLeft, 1000);
+			promise = $interval(function(){
+				if($scope.timeLeft < 1){
+					if($scope.currentName == $scope.sessionName) {
+						$scope.currentName = $scope.breakName;
+						console.log($scope.currentName);
+						$scope.timeLeft = $scope.breakTime;
+					} else if($scope.currentName == $scope.breakName) {
+						$scope.currentName = $scope.sessionName;
+						console.log($scope.currentName);
+						$scope.timeLeft = $scope.sessionTime;
+					}
+				}
+				$scope.minusTimeLeft();
+			}, 1000);
+
 		} else if($scope.isRunning === false) {
 			$interval.cancel(promise);
-		}
-		
-		
+		}	
 	}
+
+	$scope.updateTimer = function(){
+		console.log("Running Update Timer");
+		if($scope.timeLeft < 1){
+			if($scope.currentName == $scope.sessionName) {
+				$scope.currentName == $scope.breakName;
+				//console.log($scope.currentName);
+				$scope.timeLeft = $scope.breakTime;
+			} else {
+				$scope.currentName == $scope.sessionName;
+				//console.log($scope.currentName);
+				$scope.timeLeft = $scope.sessionTime;
+			}
+		}
+	}
+
+
+
+
+	// $scope.updateTimer = function(){
+	// 	if($scope.name == "Session"){
+	// 		$scope.timeLeft = $scope.sessionTime;
+	// 		$scope.minusTimeLeft();
+	// 		promise =$interval($scope.minusTimeLeft, 1000);
+	// 		if($scope.timeLeft < 1) {
+	// 			$scope.name = "Break";
+	// 		}
+	// 	}
+	// 	if ($scope.name == "Break");
+	// 		$scope.timeLeft = $scope.breakTime;
+	// 		$scope.minusTimeLeft();
+	// 		promise =$interval($scope.minusTimeLeft, 1000);
+	// 		if($scope.timeLeft < 1) {
+	// 			$scope.name = "Session";
+	// 		}
+	// }
 
 
 
